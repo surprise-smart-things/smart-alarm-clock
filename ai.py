070f0f79fe8f0c38bac796a713365605296277ee
Navaneeth Sivakumar <navaneeth.sivakumar66@gmail.com>
2023-09-02 12:16:12 +0530

    added step to model

--- ai.py
+++ ai.py
@@ -1,16 +1,18 @@
 import pickle
 import numpy as np
 import warnings
-import tensorflow.keras
+import keras
 warnings.filterwarnings("ignore")
 
-def giveSnooze(new_data, wake):
+def giveSnooze(new_data, step, wake ):
     # AI calculates and returns number of times user will snooze
-    filename = 'model_v1.pkl'
+    filename = 'model_v3.keras'
     new_data = list(new_data)
-    new_data[-1] = wake
+    new_data.append(step)
+    print(new_data)
+    new_data[-2] = wake
     new_data = np.array(new_data).reshape(1, -1)
-    loaded_model = pickle.load(open(filename, 'rb'))
+    loaded_model = keras.models.load_model(filename)
     prediction = loaded_model.predict(new_data)
     return (np.where(prediction[0] >= 0.5)[0])
 
